7c8119b4c05fba62e32c7dc8c4578d8da0394a33
zerokaze <erfu81856@gmail.com>
2025-05-07 05:59:18 +0000

    Add flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
